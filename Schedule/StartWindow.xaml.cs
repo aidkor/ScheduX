e4b6cb2b996516c348689780b3f7d2d42ccb31e2
Jojo <denyskoren88@gmail.com>
2021-02-12 23:34:11 +0200

    first commit

---
+++ Schedule/StartWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Schedule
+{
+    /// <summary>
+    /// Interaction logic for StartWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+            Width = SystemParameters.PrimaryScreenWidth / 4;
+            Height = SystemParameters.PrimaryScreenHeight / 4 + 40;            
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            CloseWindow();            
+        }
+
+        private async void CloseWindow()
+        {
+            await ClosingTasks();
+        }
+
+        private async Task ClosingTasks()
+        {
+            await Task.Delay(2000);
+            Close();
+        }
+
+    }
+}
