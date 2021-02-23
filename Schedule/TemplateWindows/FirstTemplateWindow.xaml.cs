d5e7c857a8b4a688d69bf74587412618270827d4
Jojo <denyskoren88@gmail.com>
2021-02-24 00:25:30 +0200

    UI

---
+++ Schedule/TemplateWindows/FirstTemplateWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Schedule.TemplateWindows
+{
+    /// <summary>
+    /// Interaction logic for FirstTemplateWindow.xaml
+    /// </summary>
+    public partial class FirstTemplateWindow : Window
+    {
+        public FirstTemplateWindow()
+        {
+            InitializeComponent();
+            // HACK: Change this in XAML code with data binding
+            this.Height = SystemParameters.PrimaryScreenHeight / 2;
+            this.Width = SystemParameters.PrimaryScreenWidth / 5;
+        }
+        private void CreateNewProject(object sender, RoutedEventArgs e)
+        {
+            new SecondTemplateWindow().Show();
+            Close();
+        }
+
+        private void OpenProject(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ProjectTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+     
+    }
+}
---
+++ Schedule/TemplateWindows/SecondTemplateWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Schedule.TemplateWindows
+{
+    /// <summary>
+    /// Interaction logic for FirstTemplateWindow.xaml
+    /// </summary>
+    public partial class SecondTemplateWindow : Window
+    {
+        public SecondTemplateWindow()
+        {
+            InitializeComponent();
+            // HACK: Change this in XAML code with data binding
+            this.Height = SystemParameters.PrimaryScreenHeight / 2;
+            this.Width = SystemParameters.PrimaryScreenWidth / 5;
+        }
+
+        private void SchoolTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            new StartWindow().Show();
+            Close();
+        }
+    }
+}
