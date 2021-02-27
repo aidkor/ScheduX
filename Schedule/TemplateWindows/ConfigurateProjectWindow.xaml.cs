75af3c790b50b85acb07563d613938039ef07eda
Jojo <denyskoren88@gmail.com>
2021-02-27 15:55:57 +0200

    add logic for ConfigurateProjectWindow/CreateOpenProjectWindow

---
+++ Schedule/TemplateWindows/ConfigurateProjectWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Schedule.Resourses;
+
+
+namespace Schedule.TemplateWindows
+{
+    enum ProjectTemplate : byte
+    {
+        Kindergarten = 0,
+        SchoolTemplate = 1,
+        UniversityTemplate = 2
+    }
+    /// <summary>
+    /// Interaction logic for FirstTemplateWindow.xaml
+    /// </summary>
+    public partial class ConfigurateProjectWindow : Window
+    {
+        private ProjectTemplate template;
+        public ConfigurateProjectWindow()
+        {
+            InitializeComponent();           
+
+            // HACK: Change this in XAML code with data binding
+            this.Height = SystemParameters.PrimaryScreenHeight / 2;
+            this.Width = SystemParameters.PrimaryScreenWidth / 5;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            IconHelper.RemoveIcon(this);
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            new CreateOpenProjectWindow().Show();
+            Close();
+        }
+
+        private void SearchPathButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    LocationTextBox.Text = fbd.SelectedPath;
+                }
+            }
+        }
+        private void CreateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProjectNameTextBox.Text != "" & Directory.Exists(LocationTextBox.Text) & IsTemplateSelected())
+            {
+                // TODO: Add logic for creating project folder/files
+
+                new EditorWindow().Show();
+                Close();
+            }            
+
+           
+        }
+
+        private void SchoolTemplate_Selected(object sender, RoutedEventArgs e)
+        {
+            template = ProjectTemplate.SchoolTemplate;
+        }
+        private bool IsTemplateSelected()
+        {
+            for (int i = 0; i < TemplateListBox.Items.Count; i++)
+            {
+                if (((ListBoxItem)TemplateListBox.Items[i]).IsSelected)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ TestFunc/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProjectTextBox_TextChanged();
+            Console.ReadKey();
+        }
+
+
+
+        private static void ProjectTextBox_TextChanged()
+        {
+            string scheduxProjectDir = @"C:\Users\Asus\Desktop\Proj";
+            ArrayList shxFiles = new ArrayList();
+            SearchSystem(scheduxProjectDir, ref shxFiles);
+            for (int i = 0; i < shxFiles.Count; i++)
+            {
+                Console.WriteLine(shxFiles[i]);
+            }
+        }
+        private static void SearchSystem(string dirPath, ref ArrayList files)
+        {
+            try
+            {
+                if (Directory.Exists(dirPath))
+                {
+                    foreach (string item in FindShxFile(Directory.GetFiles(dirPath)))
+                    {
+                        files.Add(item);
+                    }
+                    foreach (string item in Directory.GetDirectories(dirPath))
+                    {
+                        SearchSystem(item, ref files);
+                    }
+                }
+            }
+            catch
+            {              
+                return;                
+            }
+
+        }
+        private static ArrayList FindShxFile(string[] files)
+        {
+            ArrayList result = new ArrayList();
+
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (files[i].Split('.')[files[i].Split('.').Length - 1] == "shx")
+                {
+                    result.Add(files[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+}
